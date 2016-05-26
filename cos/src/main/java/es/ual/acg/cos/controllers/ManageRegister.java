644acb28136d8dbff6e197b51364d175fc222e35
alfredo <alfredo@mini>
2016-05-26 09:51:50 +0200

    Mensaje de Licencia en Controladores

--- cos/src/main/java/es/ual/acg/cos/controllers/ManageRegister.java
+++ cos/src/main/java/es/ual/acg/cos/controllers/ManageRegister.java
@@ -1,3 +1,36 @@
+/*
+ * ManageRegister.java -- Gestiona la base de datos de modelos de arquitecturas de las aplicaciones mashup.
+ * Copyright (C) 2016  Alfredo Valero Rodríguez, Javier Criado Rodríguez and Jesús Vallecillos Ruíz
+ *
+ * ManageRegister.java is part of COScore Community.
+ * 
+ * COScore Community is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/gpl.html> or
+ * see  <https://github.com/acgtic211/COScore-Community>.  Or write to
+ * the Free Software Foundation, I51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1335, USA.
+ *
+ *  Authors: Alfredo Valero Rodríguez  Software Developer 
+ *           Javier Criado Rodríguez   Doctor/Researcher/Software Developer
+ *           Jesús Vallecillos Ruíz    Pre-doctoral scholarship holders/Researcher/Software Developer
+ *    Group: ACG 		               Applied Computing Group
+ * Internet: http://acg.ual.es/        
+ *   E-mail: acg.tic211@ual.es        
+ *   Adress: Edif. Científico Técnico, CITE-III
+ *           Universidad de Almería
+ *           Almeria, España
+ *           04120
+*/
 package es.ual.acg.cos.controllers;
 
 import java.io.IOException;
